a4a92efb4130388f7d1d02aa848daa281ff5e516
tanjil14 <tanjil14@cse.pstu.ac.bd>
2024-01-06 19:31:39 +0600

    create top rated section

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -4,22 +4,22 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
-const Trending = () => {
-  const [endpoint, setEndPoint] = useState("day");
-  const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+const Popular = () => {
+  const [endpoint, setEndPoint] = useState("movie");
+  const { data, loading } = useFetch(`/${endpoint}/popular`);
 
   const onTabChange = (tab) => {
-    setEndPoint(tab === "Day" ? "day" : "week");
+    setEndPoint(tab === "Movies" ? "movie" : "tv");
   };
   return (
     <div className="carouselSection">
       <ContentWrapper>
-        <span className="carouselTitle">Trending</span>
-        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+        <span className="carouselTitle">What's Popular</span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
       </ContentWrapper>
       <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
   );
 };
 
-export default Trending;
+export default Popular;
