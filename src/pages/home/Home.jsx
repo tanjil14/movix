cccf0e6f5083965ed832c2d7a7900d5d967b3423
tanjil14 <tanjil14@cse.pstu.ac.bd>
2024-01-06 14:27:58 +0600

    create shitch tab

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./style.scss";
 
 import HeroBanner from "./heroBanner/HeroBanner";
-// import Trending from "./trending/Trending";
+import Trending from "./trending/Trending";
 // import Popular from "./popular/Popular";
 // import TopRated from "./topRated/TopRated";
 
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
-      {/* <Trending />
-            <Popular />
+      <Trending />
+      {/*     <Popular />
             <TopRated /> */}
     </div>
   );
